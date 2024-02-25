8e7a8da675632d4398038bdadaca379f3f1281f9
Mark Karpeles <magicaltux@gmail.com>
2024-02-25 12:44:06 +0900

    clone: no need to draw the original image, and attempt to keep the image type

--- clone.go
+++ clone.go
@@ -6,9 +6,26 @@ import (
 )
 
 // CloneImage simply copies the given image in a new image.NRGBA
-func CloneImage(img image.Image) *image.NRGBA {
+func CloneImage(img image.Image) draw.Image {
 	rect := img.Bounds()
-	res := image.NewNRGBA(rect)
-	draw.Draw(res, rect, img, image.ZP, draw.Src)
-	return res
+	switch img.(type) {
+	case *image.Alpha:
+		return image.NewAlpha(rect)
+	case *image.Alpha16:
+		return image.NewAlpha16(rect)
+	case *image.Gray:
+		return image.NewGray(rect)
+	case *image.Gray16:
+		return image.NewGray16(rect)
+	case *image.NRGBA:
+		return image.NewNRGBA(rect)
+	case *image.NRGBA64:
+		return image.NewNRGBA64(rect)
+	case *image.RGBA:
+		return image.NewRGBA(rect)
+	case *image.RGBA64:
+		return image.NewRGBA64(rect)
+	default:
+		return image.NewNRGBA(rect)
+	}
 }
